12836b87d3c89c6cd824371c88531b8ccddaa9da
icvvmusicschool-icvv <icvvmusicschool@gmail.com>
2026-03-05 07:51:36 -0300

    Update dashboard_home_screen.dart

--- lib/presentation/dashboard_home_screen/dashboard_home_screen.dart
+++ lib/presentation/dashboard_home_screen/dashboard_home_screen.dart
@@ -1,1 +1,65 @@
 
+import 'package:flutter/material.dart';
+
+import '../../routes/app_routes.dart';
+import '../../widgets/custom_bottom_bar.dart';
+import './dashboard_home_screen_initial_page.dart';
+
+class DashboardHomeScreen extends StatefulWidget {
+  const DashboardHomeScreen({super.key});
+
+  @override
+  DashboardHomeScreenState createState() => DashboardHomeScreenState();
+}
+
+class DashboardHomeScreenState extends State<DashboardHomeScreen> {
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  int currentIndex = 0;
+
+  final List<String> routes = [
+    '/dashboard-home-screen',
+    '/cadastro-musicos-screen',
+    '/escalas-management-screen',
+    '/events-management-screen',
+    '/music-library-screen',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Navigator(
+        key: navigatorKey,
+        initialRoute: '/dashboard-home-screen',
+        onGenerateRoute: (settings) {
+          switch (settings.name) {
+            case '/dashboard-home-screen' || '/':
+              return MaterialPageRoute(
+                builder: (context) => const DashboardHomeScreenInitialPage(),
+                settings: settings,
+              );
+            default:
+              if (AppRoutes.routes.containsKey(settings.name)) {
+                return MaterialPageRoute(
+                  builder: AppRoutes.routes[settings.name]!,
+                  settings: settings,
+                );
+              }
+              return null;
+          }
+        },
+      ),
+      bottomNavigationBar: CustomBottomBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          if (!AppRoutes.routes.containsKey(routes[index])) {
+            return;
+          }
+          if (currentIndex != index) {
+            setState(() => currentIndex = index);
+            navigatorKey.currentState?.pushReplacementNamed(routes[index]);
+          }
+        },
+      ),
+    );
+  }
+}
